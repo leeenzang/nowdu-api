dcceff4f14a4a3662378bc8772820a02593d3632
LEE EUNJIN <127672852+leeenzang@users.noreply.github.com>
2026-02-21 17:39:58 +0900

    init

---
+++ src/main/java/com/tauceti/nowdu/NowduApplication.java
@@ -0,0 +1,13 @@
+package com.tauceti.nowdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tauceti/nowdu/NowduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tauceti.nowdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
